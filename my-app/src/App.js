03518fadbf0003157038bb97738d04cf0eae5aa6
Annalise Murphy <nali247@gmail.com>
2020-08-03 15:22:44 -0700

    header added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Delicious</div>
+        <div>
+          <p> 
+            <img src='./assets/fb-icon.png' alt="facebook" />
+            <img src='./assets/twit-icon.png' alt="twitter" />
+            <img src="./assets/gp-icon.png" alt="google-plus"/>
+            <img src="./assets/insta-icon.png" alt="instagram"/>
+            <img src="./assets/flic-icon.png" alt="flicker" />
+            <img src="./assets/pint-icon.png" alt="pintrest" />
+          </p>
+          <p>
+            <img src="./assets/rss-icon.png" alt="rss" />
+            <img src="./assets/mail-icon.png" alt="mail" />
+          </p>
+
+        </div>
+        <div>
+          <div>
+            <img src="./assets/hr-img.png" alt="hr" />
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// need to add buttons to the header
+
+// main 
+export default class AllPutTogether extends React.Component {
+  render() {
+    return(
+      <header>
+        <Header />
+      </header>
+    )
+  }
+}
+
+
+
